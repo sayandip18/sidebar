02bd21f97221d455be870ce02aa5d16c0bc6082a
Sayandip Halder <sayandiph4@gmail.com>
2022-04-30 17:47:53 +0530

    css improvements, data fetching from API

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import About from '../pages/about/About';
+import Contact from '../pages/contact/Contact';
+import Home from '../pages/home/Home';
 
 import './Body.css';
 
 function Body() {
     return (
         <div className="body">
-            Body
+            <Home />
         </div>
     )
 }
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function Home() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get('https://api.publicapis.org/entries');
+            setData(response.data.entries);
+        }
+        fetchData();
+        
+    })
+
     return (
-        <div>Home</div>
+        <div style={{height: '100%', overflowX: 'scroll'}}>
+            {
+                data.slice(0,20).map(
+                    (item, id) =>
+                    <div key={id}>
+                        <div>
+                            <span style={{fontWeight: 'bold', marginRight: '10px'}}>{id}</span>
+                            API: {item.API}
+                        </div>
+                        <div style={{marginBottom: 10}}>Link: {item.Link}</div>
+                    </div>
+                )
+            }
+        </div>
     )
 }
 
