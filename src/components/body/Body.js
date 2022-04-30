9da94b25db73aad85b95eece5d2f0d8894ae121c
Sayandip Halder <sayandiph4@gmail.com>
2022-04-30 19:47:55 +0530

    add feedback system

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import About from '../pages/about/About';
 import Contact from '../pages/contact/Contact';
+import Feedback from '../pages/feedback/Feedback';
 import Home from '../pages/home/Home';
 
 import './Body.css';
@@ -8,7 +9,7 @@ import './Body.css';
 function Body() {
     return (
         <div className="body">
-            <Home />
+            <Feedback />
         </div>
     )
 }
--- src/components/pages/feedback/Feedback.js
+++ src/components/pages/feedback/Feedback.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {AiFillStar, AiOutlineStar} from "react-icons/ai";
+
+import './Feedback.css';
 
 function Feedback() {
+    const [rating, setRating] = useState(0);
+
+    const handleClick = (index) => {
+        setRating(index);
+        console.log(index);
+    }
+
     return (
-        <div>Feedback</div>
+        <div className="feedback">
+            {[...Array(5)].map((star, index) => {
+                index += 1;
+                return (
+                <div
+                    key={index}
+                    onClick={() => handleClick(index)}
+                >
+                    {
+                        index <= rating? <AiFillStar size={30} /> : <AiOutlineStar size={30} />
+                    }
+                </div>
+                );
+            })}
+        </div>
     )
 }
 
