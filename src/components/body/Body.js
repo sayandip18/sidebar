31ccb55d52cfaa5cef37d0c0b28c76293b2f93b5
Sayandip Halder <sayandiph4@gmail.com>
2022-04-30 16:04:09 +0530

    remove unnecessary files, create sidebar skeleton

---
+++ src/components/body/Body.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Body.css';
+
+function Body() {
+    return (
+        <div className="body">
+            Body
+        </div>
+    )
+}
+
+export default Body;
