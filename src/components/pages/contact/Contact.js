d9fd2cc3965caba8d6fb91b9f111cb2b23f68b9b
Sayandip Halder <sayandiph4@gmail.com>
2022-04-30 16:34:45 +0530

    add home, contact, about, feedback pages and improve sidebar

---
+++ src/components/pages/contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Contact.css';
+
+function Contact() {
+    return (
+        <div className="contact">
+            <h1>Contact</h1>
+        </div>
+    )
+}
+
+export default Contact;
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -5,7 +5,10 @@ import './Sidebar.css';
 function Sidebar() {
     return (
         <div className="sidebar">
-            Sidebar
+            <div style={{marginTop: 50}}>Home</div>
+            <div style={{marginTop: 10}}>About</div>
+            <div style={{marginTop: 10}}>Contact</div>
+            <div style={{marginTop: 10}}>Feedback</div>
         </div>
     )
 }
